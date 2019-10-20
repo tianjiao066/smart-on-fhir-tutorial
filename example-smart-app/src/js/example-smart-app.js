c936421a827f9fdbbf5f699464a34e6b2fd9eab9
tianjiao066 <38390065+tianjiao066@users.noreply.github.com>
2019-10-20 19:59:34 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -81,7 +81,7 @@
       gender: {value: ''},
       birthdate: {value: ''},
       height: {value: ''},
-      weight:{value: ''},
+      weight: {value: ''},
       systolicbp: {value: ''},
       diastolicbp: {value: ''},
       ldl: {value: ''},
